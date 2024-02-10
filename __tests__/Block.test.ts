c140ed9ce2ac7841cbd292345fdafec328e506d6
Mauricio <mauricio.tp_@outlook.com>
2024-02-10 19:09:47 -0300

    feat: create chain

--- __tests__/Block.test.ts
+++ __tests__/Block.test.ts
@@ -1,41 +1,48 @@
 import Block from '../src/lib/Block';
 
 describe('Block tests', () => {
+  let genesis: Block;
+
+  beforeAll(() => {
+    genesis = new Block(0, '', 'Genesis Block');
+  });
+
   test('Should be valid', () => {
-    const block = new Block(0, 'abc', 'data');
-    const valid = block.isValid();
+    const block = new Block(1, genesis.hash, 'block 2');
+    const valid = block.isValid(genesis.hash, genesis.index);
     expect(valid).toBeTruthy();
   });
 
-  test('Should NOT be valid (index)', () => {
-    const block = new Block(-1, 'abc', 'data');
-    const valid = block.isValid();
-    expect(valid).toBeFalsy();
-  });
-
-  test('Should NOT be valid (hash)', () => {
-    const block = new Block(0, 'abc', 'data');
-    block.hash = '';
-    const valid = block.isValid();
-    expect(valid).toBeFalsy();
-  });
-
-  test('Should NOT be valid (data)', () => {
-    const block = new Block(0, 'abc', '');
-    const valid = block.isValid();
+  test('Should NOT be valid (previous hash)', () => {
+    const block = new Block(1, 'abc', 'block 2');
+    const valid = block.isValid(genesis.hash, genesis.index);
     expect(valid).toBeFalsy();
   });
 
   test('Should NOT be valid (timestamp)', () => {
-    const block = new Block(0, 'abc', 'data');
-    block.timestamp = 0;
-    const valid = block.isValid();
+    const block = new Block(1, genesis.hash, 'block 2');
+    block.timestamp = -1;
+    block.hash = block.getHash();
+    const valid = block.isValid(genesis.hash, genesis.index);
     expect(valid).toBeFalsy();
   });
 
-  test('Should NOT be valid (previous hash)', () => {
-    const block = new Block(0, '', 'data');
-    const valid = block.isValid();
+  test('Should NOT be valid (hash)', () => {
+    const block = new Block(1, genesis.hash, 'block 2');
+    block.hash = '';
+    const valid = block.isValid(genesis.hash, genesis.index);
+    expect(valid).toBeFalsy();
+  });
+
+  test('Should NOT be valid (data)', () => {
+    const block = new Block(1, genesis.hash, '');
+    const valid = block.isValid(genesis.hash, genesis.index);
+    expect(valid).toBeFalsy();
+  });
+
+  test('Should NOT be valid (index)', () => {
+    const block = new Block(-1, genesis.hash, 'block 2');
+    const valid = block.isValid(genesis.hash, genesis.index);
     expect(valid).toBeFalsy();
   });
 });
--- __tests__/Blockchain.test.ts
+++ __tests__/Blockchain.test.ts
@@ -1,8 +1,42 @@
+import Block from '../src/lib/Block';
 import Blockchain from '../src/lib/Blockchain';
 
 describe('Blockchain tests', () => {
-  test('should has genesis block', () => {
+  test('Should has genesis block', () => {
     const blockchain = new Blockchain();
     expect(blockchain.blocks.length).toEqual(1);
+  });
+
+  test('Should be valid (genesis)', () => {
+    const blockchain = new Blockchain();
+    expect(blockchain.isValid()).toEqual(true);
+  });
+
+  test('Should be valid (two blocks)', () => {
+    const blockchain = new Blockchain();
+    blockchain.addBlock(new Block(1, blockchain.blocks[0].hash, 'block 2'));
+    expect(blockchain.isValid()).toEqual(true);
+  });
+
+  test('Should NOT be valid', () => {
+    const blockchain = new Blockchain();
+    blockchain.addBlock(new Block(1, blockchain.blocks[0].hash, 'block 2'));
+    blockchain.blocks[1].data = 'a transfere 2 para b';
+    expect(blockchain.isValid()).toEqual(false);
+  });
+
+  test('Should add block', () => {
+    const blockchain = new Blockchain();
+    const result = blockchain.addBlock(
+      new Block(1, blockchain.blocks[0].hash, 'block 2')
+    );
+    expect(result).toEqual(true);
+  });
+
+  test('Should NOT add block', () => {
+    const blockchain = new Blockchain();
+    const block = new Block(-1, blockchain.blocks[0].hash, 'block 2');
+    const result = blockchain.addBlock(block);
+    expect(result).toEqual(false);
   });
 });
