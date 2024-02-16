5a44c9af991867417d2e8704c179b57eb0199d4e
Mauricio <mauricio.tp_@outlook.com>
2024-02-16 12:15:03 -0300

    feat: create miner client

--- src/client/minerClient.ts
+++ src/client/minerClient.ts
@@ -1,11 +1,41 @@
 import axios from 'axios';
+import BlockInfo from '../lib/blockInfo';
+import Block from '../lib/block';
 
 const BLOCKCHAIN_SERVER = 'http://localhost:3000/';
+const minerWallet = {
+  privateKey: '123456',
+  publicKey: 'luiztools',
+};
+
+let totalMined = 0;
 
 async function mine() {
+  console.log('Getting next block info...');
   const { data } = await axios.get(`${BLOCKCHAIN_SERVER}blocks/next`);
+  const blockInfo = data as BlockInfo;
 
-  console.log(data);
+  const newBlock = Block.fromBlockInfo(blockInfo);
+
+  //TODO: Adicionar tx de recompensa
+
+  console.log('Start mining block #' + blockInfo.index);
+  newBlock.mine(blockInfo.difficulty, minerWallet.publicKey);
+
+  console.log('Block mined! Sending to blockchain...');
+
+  try {
+    await axios.post(`${BLOCKCHAIN_SERVER}blocks/`, newBlock);
+    console.log('Block sent and accepted!');
+    totalMined++;
+    console.log('Total mined blocks: ' + totalMined);
+  } catch (err: any) {
+    console.error(err.response ? err.response.data : err.message);
+  }
+
+  setTimeout(() => {
+    mine();
+  }, 1000);
 }
 
 mine();
