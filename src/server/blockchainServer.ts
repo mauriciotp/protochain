2cc44c96307d7278a95daec29b14637d073b24dc
Mauricio <mauricio.tp_@outlook.com>
2024-02-21 10:49:07 -0300

    feat: create transactions methods

--- src/server/blockchainServer.ts
+++ src/server/blockchainServer.ts
@@ -5,6 +5,7 @@ import express, { NextFunction, Request, Response } from 'express';
 import morgan from 'morgan';
 import Blockchain from '../lib/blockchain';
 import Block from '../lib/block';
+import Transaction from '../lib/transaction';
 
 /* c8 ignore next */
 const PORT: number = parseInt(`${process.env.BLOCKCHAIN_PORT || 3000}`);
@@ -51,6 +52,28 @@ app.post('/blocks', (req: Request, res: Response, next: NextFunction) => {
   const validation = blockchain.addBlock(block);
 
   if (validation.success) return res.status(201).json(block);
+  else return res.status(400).json(validation);
+});
+
+app.get(
+  '/transactions/:hash?',
+  (req: Request, res: Response, next: NextFunction) => {
+    if (req.params.hash) res.json(blockchain.getTransaction(req.params.hash));
+    else
+      res.json({
+        next: blockchain.mempool.slice(0, Blockchain.TX_PER_BLOCK),
+        total: blockchain.mempool.length,
+      });
+  }
+);
+
+app.post('/transactions', (req: Request, res: Response, next: NextFunction) => {
+  if (req.body.hash === undefined) return res.sendStatus(422);
+
+  const tx = new Transaction(req.body as Transaction);
+  const validation = blockchain.addTransaction(tx);
+
+  if (validation.success) return res.status(201).json(tx);
   else return res.status(400).json(validation);
 });
 
