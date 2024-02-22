bc464d459bd3eccb5f851aa777b6913d81e01021
Mauricio <mauricio.tp_@outlook.com>
2024-02-22 16:25:41 -0300

    feat: create transactionInput class

--- src/lib/transaction.ts
+++ src/lib/transaction.ts
@@ -1,6 +1,7 @@
 import TransactionType from './transactionType';
 import sha256 from 'crypto-js/sha256';
 import Validation from './validation';
+import TransactionInput from './transactionInput';
 
 /**
  * Transaction class
@@ -9,23 +10,28 @@ export default class Transaction {
   type: TransactionType;
   timestamp: number;
   hash: string;
-  data: string;
+  txInput: TransactionInput;
+  to: string;
 
   constructor(tx?: Transaction) {
     this.type = tx?.type || TransactionType.REGULAR;
     this.timestamp = tx?.timestamp || Date.now();
-    this.data = tx?.data || '';
+    this.to = tx?.to || '';
+    this.txInput = new TransactionInput(tx?.txInput) || new TransactionInput();
     this.hash = tx?.hash || this.getHash();
   }
 
   getHash(): string {
-    return sha256(this.type + this.data + this.timestamp).toString();
+    return sha256(
+      this.type + this.txInput.getHash() + this.to + this.timestamp
+    ).toString();
   }
 
   isValid(): Validation {
     if (this.hash !== this.getHash())
       return new Validation(false, 'Invalid hash.');
-    if (!this.data) return new Validation(false, 'Invalid data.');
+
+    if (!this.to) return new Validation(false, 'Invalid to.');
 
     return new Validation();
   }
